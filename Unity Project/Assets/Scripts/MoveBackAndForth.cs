c428a9c912a79dbb4837deb9a8c9bea03c0cfae6
Mathias Kahl <mathias.kahl@gmail.com>
2014-12-13 14:51:31 +0100

    Test scene with moving platforms

--- Unity Project/Assets/Scripts/MoveBackAndForth.cs
+++ Unity Project/Assets/Scripts/MoveBackAndForth.cs
@@ -5,34 +5,49 @@ public class MoveBackAndForth : MonoBehaviour {
 
 	public Vector3 StartOffset;
 	public Vector3 EndOffset;
-	public float Speed = 0.005f;
+	public float Speed = 1;
+	public float RestingDurationMs = 250;
+	public float DelayMs = 0;
 
 	private float t = 0;
 	private bool f = false;
 	private Vector3 StartPos;
 	private float distance;
+	private float wait = 0; // seconds
 
 	// Use this for initialization
 	void Start () {
 		this.StartPos = this.transform.position;
 		this.distance = (this.EndOffset - this.StartOffset).magnitude;
+		this.wait = this.DelayMs / 1000.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (this.f) {			
-			t += this.Speed;
-			if (t >= this.distance) this.f = !this.f;			
-		}
-		else {
-			t -= this.Speed;
-			if (t <= 0) this.f = !this.f;
+		if (wait <= 0)
+		{
+			if (this.f) {			
+				t += this.Speed * Time.deltaTime;
+				if (t >= this.distance) 
+				{
+					this.f = !this.f;
+					this.wait = this.RestingDurationMs / 1000.0f;			
+				}
+			}
+			else {
+				t -= this.Speed * Time.deltaTime;
+				if (t <= 0) 
+				{
+					this.f = !this.f;
+					this.wait = this.RestingDurationMs / 1000.0f;
+				}
+			}
 		}
 
-		Vector3 dir = (this.StartOffset - this.EndOffset);
-		float length = dir.magnitude;
+		Vector3 dir = (this.EndOffset - this.StartOffset);		
 		dir.Normalize();
 
-		this.transform.position = this.StartPos - this.StartOffset + dir * this.t;
+		if (wait <= 0) this.transform.position = this.StartPos - this.StartOffset + dir * this.t;
+		else wait -= Time.deltaTime;
 	}
 }
