59d3651d2ca22929b0a50a8a7bf688126a4a4fc6
Jan Sandbrink <fischikowski@web.de>
2014-12-13 17:48:21 +0100

    some jumpi experiments... not too great now

---
+++ Unity Project/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPad : MonoBehaviour {
+    public Vector3 JumpVector
+    {
+        get { return JumpDirection * JumpVelocity; }
+    }
+
+    public Vector3 JumpDirection = Vector3.up;
+
+    public float JumpVelocity = 10f;
+}
---
+++ Unity Project/Assets/Scripts/JumpPadAffectable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPadAffectable : MonoBehaviour
+{
+    private CharacterMotor myMotor;
+
+    void Start()
+    {
+        myMotor = this.GetComponent<CharacterMotor>();
+
+        if (myMotor == null)
+        {
+            Debug.LogError("Object without Character Motor tries to be JumpPadAffectable", this);
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var jumpPad = hit.gameObject.GetComponent<JumpPad>();
+        if (jumpPad != null)
+        {
+            Debug.Log("Weeeeeeeeeeeee!");
+            this.myMotor.movement.velocity += jumpPad.JumpVector;
+        }
+    }
+}
