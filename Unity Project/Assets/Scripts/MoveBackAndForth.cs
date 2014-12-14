7bd5894488161827a0b576f0ee0b35299008e924
Jan Sandbrink <fischikowski@web.de>
2014-12-14 16:15:56 +0100

    fix move back and forth
    was apparently broken for StartOffset != (0, 0, 0)
    
    should not affect Level1, all starts there are zero'ed

--- Unity Project/Assets/Scripts/MoveBackAndForth.cs
+++ Unity Project/Assets/Scripts/MoveBackAndForth.cs
@@ -9,45 +9,49 @@ public class MoveBackAndForth : MonoBehaviour {
 	public float RestingDurationMs = 250;
 	public float DelayMs = 0;
 
-	private float t = 0;
-	private bool f = false;
-	private Vector3 StartPos;
+	private float travelledDistance = 0;
+	private bool moveForward = false;
+	private Vector3 StartPosition;
+    private Vector3 EndPosition;
+    private Vector3 travelDirection;
 	private float distance;
-	private float wait = 0; // seconds
+	private float remainingWaitSeconds = 0; // seconds
 
 	// Use this for initialization
 	void Start () {
-		this.StartPos = this.transform.position;
-		this.distance = (this.EndOffset - this.StartOffset).magnitude;
-		this.wait = this.DelayMs / 1000.0f;
+		this.StartPosition = this.transform.position + this.StartOffset;
+        this.EndPosition = this.transform.position + this.EndOffset;
+        this.distance = (this.EndPosition - this.StartPosition).magnitude;
+        this.travelDirection = (this.EndPosition - this.StartPosition).normalized;
+		this.remainingWaitSeconds = this.DelayMs / 1000.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (wait <= 0)
+		if (remainingWaitSeconds <= 0)
 		{
-			if (this.f) {			
-				t += this.Speed * Time.deltaTime;
-				if (t >= this.distance) 
+			if (this.moveForward) {			
+				travelledDistance += this.Speed * Time.deltaTime;
+				if (travelledDistance >= this.distance) 
 				{
-					this.f = !this.f;
-					this.wait = this.RestingDurationMs / 1000.0f;			
+					this.moveForward = !this.moveForward;
+					this.remainingWaitSeconds = this.RestingDurationMs / 1000.0f;			
 				}
 			}
 			else {
-				t -= this.Speed * Time.deltaTime;
-				if (t <= 0) 
+				travelledDistance -= this.Speed * Time.deltaTime;
+				if (travelledDistance <= 0) 
 				{
-					this.f = !this.f;
-					this.wait = this.RestingDurationMs / 1000.0f;
+					this.moveForward = !this.moveForward;
+					this.remainingWaitSeconds = this.RestingDurationMs / 1000.0f;
 				}
 			}
-		}
 
-		Vector3 dir = (this.EndOffset - this.StartOffset);		
-		dir.Normalize();
-		
-		if (wait <= 0) this.transform.position = this.StartPos - this.StartOffset + dir * this.t;
-		else wait -= Time.deltaTime;
+            this.transform.position = this.StartPosition + (this.travelDirection * this.travelledDistance);
+		}
+        else
+        {
+            remainingWaitSeconds -= Time.deltaTime;
+        }
 	}
 }
